d282ebbef4e8c508742ccc7f23760a14d1e93543
lahcenegue <98424839+lahcenegue@users.noreply.github.com>
2022-10-06 20:19:16 +0100

    email_screen

--- lib/screens/email_screen.dart
+++ lib/screens/email_screen.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:helpapp/constatns/colors.dart';
 
-class EmaiScreen extends StatelessWidget {
-  const EmaiScreen({super.key});
+class EmailScreen extends StatefulWidget {
+  const EmailScreen({super.key});
 
   @override
+  State<EmailScreen> createState() => _EmailScreenState();
+}
+
+class _EmailScreenState extends State<EmailScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('المساعدة'),
+          centerTitle: true,
+          backgroundColor: AppColors.appbarColor,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: ListView(
+            children: <Widget>[
+              const TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'الاميل',
+                ),
+              ),
+              const SizedBox(height: 20),
+              const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'موضوع المساعدة',
+                ),
+              ),
+              const SizedBox(height: 20),
+              const TextField(
+                maxLines: 8,
+                textAlignVertical: TextAlignVertical.top,
+                decoration: InputDecoration(
+                  labelText: 'محتوى الطلب',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.scondColor,
+                    shadowColor: Colors.black,
+                    elevation: 10,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32.0)),
+                    minimumSize: const Size(200, 60),
+                    side: BorderSide(
+                      color: Colors.white.withOpacity(0.5),
+                    )),
+                onPressed: () {},
+                child: const Text('ارسال'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
